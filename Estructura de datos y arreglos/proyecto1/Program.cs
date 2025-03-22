34b88eb513471635b4cd6985f2d1b2504f31221d
walird <waliromano949@gmail.com>
2025-03-22 12:43:49 -0400

    Add files via upload

---
+++ Estructura de datos y arreglos/proyecto1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+      
+        string[] empleados = { "Joel Cruz", "Amanda Santana", "Alexander Martín", "Héctor Guzmán", "Milagros Catedral" };
+        string[] departamentos = { "Recursos Humanos", "Tráfico", "Área de Limpieza", "Ventas y Contabilidad", "Almacén" };
+        string[] combinados = new string[5];
+
+       
+        for (int i = 0; i < combinados.Length; i++)
+        {
+            combinados[i] = empleados[i] + " - " + departamentos[i];
+        }
+
+      
+        Console.WriteLine("Lista de empleados y departamentos:");
+        foreach (var item in combinados)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+
